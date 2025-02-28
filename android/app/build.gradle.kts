34f95bbc2b0b8a37a0a6e6ac1a220b6fee6bea07
V1n1v131r4 <v1n1v131r4@pm.me>
2025-02-28 12:33:18 -0300

    Google Play Billing added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.bunqr.prepapp"
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 5
+        versionName = "1.0.5"
     }
 
     signingConfigs {
