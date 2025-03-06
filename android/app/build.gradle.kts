c08f35329929e0da07a91b438dff05f48dc3fa1d
V1n1v131r4 <v1n1v131r4@pm.me>
2025-03-06 14:28:19 -0300

    new version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.bunqr.prepapp"
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
-        versionName = "1.0.5"
+        versionCode = 6
+        versionName = "1.0.6"
     }
 
     signingConfigs {
