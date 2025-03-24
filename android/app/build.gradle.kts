ab4ef8552876d1f49a8b94af51c9c38398b5d959
V1n1v131r4 <v1n1v131r4@pm.me>
2025-03-24 13:34:15 -0300

    finally finished training page, added signature product and etc.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.bunqr.prepapp"
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
-        versionCode = 11
-        versionName = "1.0.11"
+        versionCode = 12
+        versionName = "1.0.12"
     }
 
     signingConfigs {
